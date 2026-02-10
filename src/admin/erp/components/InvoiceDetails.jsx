b1010f26eabcaf20e1b8a4d2b00162078102aaff
MohamedBerik <monsieur.atef@gmail.com>
2026-02-10 16:26:35 +0200

    fix

--- src/admin/erp/components/InvoiceDetails.jsx
+++ src/admin/erp/components/InvoiceDetails.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import api from "../../../services/axios";
 import { notifyError } from "../../../utils/notify";
+import "./InvoiceDetails.css";
 
 const InvoiceDetails = () => {
   const { id } = useParams();

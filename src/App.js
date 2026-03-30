ea0942b29ff94ee931a70440158fb86302b36eb5
MohamedBerik <monsieur.atef@gmail.com>
2026-03-30 23:28:21 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,32 +6,22 @@ function App() {
   return (
     <div>
       <Navbar />
-      <section
-        classname="cover bg-blue-teal-gradient relative bg-blue-600 px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 overflow-hidden py-48 flex
-items-center min-h-screen"
-      >
-        <div classname="h-full absolute top-0 left-0 z-0">
-          <img
-            src="images/cover-bg.jpg"
-            alt=""
-            classname="w-full h-full object-cover opacity-20"
-          />
-        </div>
-        <div classname="lg:w-3/4 xl:w-2/4 relative z-10 h-100 lg:mt-16">
-          <div>
-            <h1 classname="text-white text-4xl md:text-5xl xl:text-6xl font-bold leading-tight">
-              A better life starts with a beautiful smile.
-            </h1>
-            <p classname="text-blue-100 text-xl md:text-2xl leading-snug mt-4">
-              Welcome to the Dentist Office of Dr. Thomas Dooley, where trust
-              and comfort are priorities.
-            </p>
-            <a
-              href="#"
-              classname="px-8 py-4 bg-teal-500 text-white rounded inline-block mt-8 font-semibold"
-            >
-              Book Appointment
-            </a>
+      <section id="home">
+        <div className="container">
+          <div className="home-content">
+            <div className="content-text">
+              <h3>Black coffee is awesome.</h3>
+              <h1>TIME DISCOVER COFFEE HOUSE</h1>
+              <p>
+                We care about quality and sustainability, sourcing ethically and
+                using eco-friendly practices-because great coffee should make a
+                difference.
+              </p>
+              <a href="#" className="btn">
+                Explore More <i className="fa-solid fa-cart-shopping" />
+              </a>
+            </div>
+            <img src="img/coffee-home.png" alt="Coffee Home" />
           </div>
         </div>
       </section>

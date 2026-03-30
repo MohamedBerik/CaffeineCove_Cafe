f8b26ee5df813f796b2b1130ad9c1c959c9a5790
MohamedBerik <monsieur.atef@gmail.com>
2026-03-30 23:30:48 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -6,22 +6,67 @@ function App() {
   return (
     <div>
       <Navbar />
-      <section id="home">
+      <section className="hero" id="hero">
         <div className="container">
-          <div className="home-content">
-            <div className="content-text">
-              <h3>Black coffee is awesome.</h3>
-              <h1>TIME DISCOVER COFFEE HOUSE</h1>
-              <p>
-                We care about quality and sustainability, sourcing ethically and
-                using eco-friendly practices-because great coffee should make a
-                difference.
-              </p>
-              <a href="#" className="btn">
-                Explore More <i className="fa-solid fa-cart-shopping" />
-              </a>
+          <div className="row">
+            <div className="col-12">
+              <div
+                id="myCarousel"
+                className="carousel slide carousel-fade"
+                data-bs-ride="carousel"
+              >
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src="images/slider/portrait-successful-mid-adult-doctor-with-crossed-arms.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src="images/slider/young-asian-female-dentist-white-coat-posing-clinic-equipment.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src="images/slider/doctor-s-hand-holding-stethoscope-closeup.jpg"
+                      className="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="heroText d-flex flex-column justify-content-center">
+                <h1 className="mt-auto mb-2">
+                  Better
+                  <div className="animated-info">
+                    <span className="animated-item">health</span>
+                    <span className="animated-item">days</span>
+                    <span className="animated-item">lives</span>
+                  </div>
+                </h1>
+                <p className="mb-4">
+                  Medic Care is a Bootstrap 5 Template provided by TemplateMo
+                  website. Credits go to FreePik and RawPixel for images used in
+                  this template.
+                </p>
+                <div className="heroLinks d-flex flex-wrap align-items-center">
+                  <a
+                    className="custom-link me-4"
+                    href="#about"
+                    data-hover="Learn More"
+                  >
+                    Learn More
+                  </a>
+                  <p className="contact-phone mb-0">
+                    <i className="bi-phone" /> 010-020-0340
+                  </p>
+                </div>
+              </div>
             </div>
-            <img src="img/coffee-home.png" alt="Coffee Home" />
           </div>
         </div>
       </section>

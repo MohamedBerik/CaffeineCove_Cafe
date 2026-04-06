da0ffece29361e67e6e85de866947ebe8752ba8e
MohamedBerik <monsieur.atef@gmail.com>
2026-04-06 18:14:33 +0200

    fix

--- src/services/echo.js
+++ src/services/echo.js
@@ -3,11 +3,18 @@ import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
+// ✅ تأكد من وجود الـ keys
+console.log("PUSHER KEY:", import.meta.env.VITE_PUSHER_KEY);
+console.log("PUSHER CLUSTER:", import.meta.env.VITE_PUSHER_CLUSTER);
+
 const echo = new Echo({
   broadcaster: "pusher",
   key: import.meta.env.VITE_PUSHER_KEY,
   cluster: import.meta.env.VITE_PUSHER_CLUSTER,
   forceTLS: true,
 });
+
+// ✅ بعد التهيئة
 window.Pusher.logToConsole = true;
+
 export default echo;

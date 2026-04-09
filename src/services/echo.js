759d3861a857852eb556f4ce7253f126a48ef8b6
MohamedBerik <monsieur.atef@gmail.com>
2026-04-09 12:49:24 +0200

    fix

--- src/services/echo.js
+++ src/services/echo.js
@@ -3,17 +3,18 @@ import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
-// ✅ تأكد من وجود الـ keys
-console.log(process.env.REACT_APP_PUSHER_KEY);
-
 const echo = new Echo({
   broadcaster: "pusher",
-  key: process.env.REACT_APP_PUSHER_KEY,
-  cluster: process.env.REACT_APP_PUSHER_CLUSTER,
+  key: "9b85ecf4278b5add7a38",
+  cluster: "mt1",
   forceTLS: true,
+  authEndpoint:
+    "https://caffeinecoveapi-production-a107.up.railway.app/broadcasting/auth", // ✅ أضف السطر ده
+  auth: {
+    headers: {
+      Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+  },
 });
-
-// ✅ بعد التهيئة
-window.Pusher.logToConsole = true;
 
 export default echo;

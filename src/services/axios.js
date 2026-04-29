a5d3a6a0e02847a169150a4540d24d3aa9662ba3
MohamedBerik <monsieur.atef@gmail.com>
2026-04-29 10:30:47 +0300

    fix

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://caffeinecoveapi-production-a107.up.railway.app/api",
+  baseURL: "/api", // ✅ صح (سيتم توجيهه عبر وكيل Vercel)
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",

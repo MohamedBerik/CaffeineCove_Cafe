b130c51970529a64687a6e97c47266bda39b7c0f
MohamedBerik <monsieur.atef@gmail.com>
2026-05-02 08:52:02 +0300

    fix

--- src/hooks/useAlertsSocket.js
+++ src/hooks/useAlertsSocket.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from "react";
-import { useAuth } from "../../../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 import echo from "../services/echo";
 import toast from "react-hot-toast";
 

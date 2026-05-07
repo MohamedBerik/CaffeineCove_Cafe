a2be6799830cf3ae2015db6fde5e9b332cec7497
MohamedBerik <monsieur.atef@gmail.com>
2026-05-07 21:04:49 +0300

    fix

--- src/admin/erp/appointments/AppointmentsListPage.jsx
+++ src/admin/erp/appointments/AppointmentsListPage.jsx
@@ -447,15 +447,14 @@ export default function AppointmentsListPage() {
     }
   };
 
-  useEffect(() => {
-    if (
-      !user?.hasPermissionTo?.("appointments.view") &&
-      !user?.hasPermissionTo?.("appointments.manage") &&
-      !user?.is_super_admin
-    ) {
-      navigate("/admin/erp/appointments", { replace: true });
-    }
-  }, [user, navigate]);
+  // useEffect(() => {
+  //   if (
+  //     !user?.hasPermissionTo?.("appointments.manage") &&
+  //     !user?.is_super_admin
+  //   ) {
+  //     navigate("/admin/erp/appointments", { replace: true });
+  //   }
+  // }, [user, navigate]);
 
   if (loading) {
     return (

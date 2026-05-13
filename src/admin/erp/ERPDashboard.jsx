09c7099fb3ef5c8b19a7e5e295a00cf11e828f53
MohamedBerik <monsieur.atef@gmail.com>
2026-05-13 18:22:20 +0300

    fix

--- src/admin/erp/ERPDashboard.jsx
+++ src/admin/erp/ERPDashboard.jsx
@@ -13,7 +13,7 @@ const ERPDashboard = () => {
 
       <div className="container-fluid" style={{ paddingTop: 86 }}>
         <div className="row g-3">
-          <div className="col-12 col-xl-3">
+          <div className="col-12 col-xl-3 d-none d-xl-block">
             <div style={{ position: "sticky", top: 86 }}>
               <ErpNav />
             </div>

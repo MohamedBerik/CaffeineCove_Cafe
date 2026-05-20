471f27540f994cf23294a5af89f20d6e51cdfd24
MohamedBerik <monsieur.atef@gmail.com>
2026-05-20 09:15:40 +0300

    fix

--- src/admin/layouts/AdminLayout.jsx
+++ src/admin/layouts/AdminLayout.jsx
@@ -1,15 +1,15 @@
-import AdminNavbar from "../components/AdminNavbar";
-import { Outlet } from "react-router-dom";
+// import AdminNavbar from "../components/AdminNavbar";
+// import { Outlet } from "react-router-dom";
 
-const AdminLayout = () => {
-  return (
-    <>
-      <AdminNavbar />
-      <main style={{ padding: "20px" }}>
-        <Outlet />
-      </main>
-    </>
-  );
-};
+// const AdminLayout = () => {
+//   return (
+//     <>
+//       <AdminNavbar />
+//       <main style={{ padding: "20px" }}>
+//         <Outlet />
+//       </main>
+//     </>
+//   );
+// };
 
-export default AdminLayout;
+// export default AdminLayout;

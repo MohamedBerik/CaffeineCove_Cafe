bedcbcbf54532df109e99ccd30b4e7cae3e14e9a
MohamedBerik <monsieur.atef@gmail.com>
2026-06-13 06:16:26 +0300

    fix

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -98,7 +98,6 @@ export function AuthProvider({ children }) {
     } catch (e) {
       // فشل logout - عادي
     }
-    echoService.disconnect();
 
     logoutLocal();
     delete api.defaults.headers.common["Authorization"];

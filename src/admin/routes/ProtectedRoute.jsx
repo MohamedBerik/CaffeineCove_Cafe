f6b4ab5989fff490d66354658aa6d37c945ba579
MohamedBerik <monsieur.atef@gmail.com>
2026-06-27 12:02:37 +0300

    fix

--- src/admin/routes/ProtectedRoute.jsx
+++ src/admin/routes/ProtectedRoute.jsx
@@ -6,7 +6,18 @@ export function AdminRoute({ children }) {
   const location = useLocation();
   const tenantId = localStorage.getItem("selectedCompany");
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return (
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  }
 
   if (!user) return <Navigate to="/login" replace />;
 
